0dddeb2ae9028681e4f49fc9a3926adf697825dd
rmara <rmara@RobertoPC>
2025-01-29 20:05:50 -0600

    added divide method

--- src/Demo1.java
+++ src/Demo1.java
@@ -5,7 +5,19 @@ public class Demo1 {
 		
 		System.out.println("***");
 		
+		int a = 20;
+		int b = 25;
+		
+		int result = a / b;
+		
+		System.out.println(result);
+		
 	}
 	
+
+	public static void add(int a, int b) {
+		
+		System.out.println("Done");
+	}
 	
 }
